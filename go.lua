512bc239323a01de72435d84432e802b6e4ab8fa
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-22 14:20:24 +0100

    Update go.lua

--- go.lua
+++ go.lua
@@ -8,9 +8,11 @@ getgenv().petsGoConfig = {
 
     CONSUME_CORRUPTED_HUGE_BAIT = true,
     CONSUME_ALL_ENCHANT_SAFE = true,
-    CONSUME_ALL_MINING_CHEST = true,
-    CONSUME_EVENT_GIFT_BAG = false,
+    CONSUME_ALL_MINING_CHEST = false,
     CONSUME_EVENT_EGG = false, -- Hype eggs not included
+    CONSUME_ALL_THIEVING_CHEST = true,
+    CONSUME_INSTA_LUCK_4 = true,
+    IGNORE_DICE_COMBO = false, -- Consume Insta Luck 3 without combo
 
     WEBHOOK_URL = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
     MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
@@ -29,19 +31,20 @@ getgenv().petsGoConfig = {
     MAIL_PET_ODDS = 100000000,  -- Minimum Pet Odds To Mail
     MAIL_GEMS_MIN = 10000000,
 
+
     MIN_MAIL_AMOUNT = {  -- Rare items not listed, default min 1
-      -- General
+  -- General
       PICKAXE_LOOT_IV = 1,
-      INSTA_POTION_IV = 100,
-      CORRUPTED_HUGE_BAIT = 50,
-      CRYSTAL_KEY = 25, CRYSTAL_KEY_UPPER_HALF = 100, CRYSTAL_KEY_LOWER_HALF = 100,
-      EXCLUSIVE_TREASURE_CHEST = 50, ABYSSAL_TREASURE_CHEST = 5,
-      CELESTIAL_MINING_CHEST = 50, RUNIC_MINING_CHEST = 250,
+      INSTA_POTION_IV = 5,
+      CORRUPTED_HUGE_BAIT = 20,
+      CRYSTAL_KEY = 5, CRYSTAL_KEY_UPPER_HALF = 10, CRYSTAL_KEY_LOWER_HALF = 20,
+      EXCLUSIVE_TREASURE_CHEST = 5, ABYSSAL_TREASURE_CHEST = 5,
+      CELESTIAL_MINING_CHEST = 5, RUNIC_MINING_CHEST = 5,
       CELESTIAL_ENCHANT_SAFE = 1,
-      -- Event
-      HUGE_CHARGE_TOKEN = 10, TITANIC_CHARGE_TOKEN = 5,
-      EVENT_GIFT_BAG = 5,
-    },  
+      EVENT_GIFT_BAG = 1,
+      EVENT_EGG = 10,
+      LEGENDARY_THIEVING_CHEST = 5,
+},
 
     USERNAME_TO_MAIL = {"TheArex2006"} -- Mail to username, Example : USERNAME_TO_MAIL = {"username1", "username2"} 
 }
