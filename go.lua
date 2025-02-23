6bdc692656beaa10d9391ede9c3ad76168041a2f
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-02-23 15:36:49 +0100

    Update go.lua

--- go.lua
+++ go.lua
@@ -4,12 +4,10 @@ script_key="FdhwIvkOLXAFPprtMaHNhatPJBEFWfwI";
 
 getgenv().petsGoConfig = {
     -- true/false
-    IGNORE_FULL_CHARGE_MEGA_EGG = false,
-
     CONSUME_CORRUPTED_HUGE_BAIT = true,
     CONSUME_ALL_ENCHANT_SAFE = true,
     CONSUME_ALL_MINING_CHEST = false,
-    CONSUME_EVENT_EGG = false, -- Hype eggs not included
+    CONSUME_EVENT_EGG = true, -- Hype eggs not included
     CONSUME_ALL_THIEVING_CHEST = true,
     CONSUME_INSTA_LUCK_4 = true,
     IGNORE_DICE_COMBO = false, -- Consume Insta Luck 3 without combo
@@ -26,25 +24,12 @@ getgenv().petsGoConfig = {
     PICKAXE_ENCHANTS = {"Loot", "Chests", ""},
     
     MAILING = false,  -- Auto mail
-    MAIL_FISHING_ROD = false,  -- true = mail, false = keep fishing rod on account (FASTER Fishing)
+    SHOW_PET_WEBHOOK_USERNAME = true,
     MAIL_WEBHOOK_ODDS = 100000000, -- Minimum Pet Odds To Trigger MAIL Webhook
     MAIL_PET_ODDS = 100000000,  -- Minimum Pet Odds To Mail
+    MAIL_FISHING_ROD = false,  -- true = mail, false = keep fishing rod on account (FASTER Fishing)
+    MAIL_PICKAXE = false, -- true = mail (smart mail [keep best pickaxe, mail excess])
     MAIL_GEMS_MIN = 10000000,
-
-
-    MIN_MAIL_AMOUNT = {  -- Rare items not listed, default min 1
-  -- General
-      PICKAXE_LOOT_IV = 1,
-      INSTA_POTION_IV = 5,
-      CORRUPTED_HUGE_BAIT = 20,
-      CRYSTAL_KEY = 5, CRYSTAL_KEY_UPPER_HALF = 10, CRYSTAL_KEY_LOWER_HALF = 20,
-      EXCLUSIVE_TREASURE_CHEST = 5, ABYSSAL_TREASURE_CHEST = 5,
-      CELESTIAL_MINING_CHEST = 5, RUNIC_MINING_CHEST = 5,
-      CELESTIAL_ENCHANT_SAFE = 1,
-      EVENT_GIFT_BAG = 1,
-      EVENT_EGG = 10,
-      LEGENDARY_THIEVING_CHEST = 5,
-},
 
     USERNAME_TO_MAIL = {"TheArex2006"} -- Mail to username, Example : USERNAME_TO_MAIL = {"username1", "username2"} 
 }
