7e74e22c488a68186e31878108ea09f09ba9da08
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-03-01 10:28:40 +0100

    Update go.lua

--- go.lua
+++ go.lua
@@ -3,24 +3,25 @@ task.wait(2)
 script_key="FdhwIvkOLXAFPprtMaHNhatPJBEFWfwI";
 
 getgenv().petsGoConfig = {
-    -- true/false
+    AUTO_RESTART = true,
+    -- true/false (true = consume, false = automail)
     CONSUME_CORRUPTED_HUGE_BAIT = true,
     CONSUME_ALL_ENCHANT_SAFE = true,
-    CONSUME_ALL_MINING_CHEST = false,
+    CONSUME_ALL_MINING_CHEST = true,
     CONSUME_EVENT_EGG = true, -- Hype eggs not included
     CONSUME_ALL_THIEVING_CHEST = true,
     CONSUME_INSTA_LUCK_4 = true,
     IGNORE_DICE_COMBO = false, -- Consume Insta Luck 3 without combo
-    MAIL_GEMS_MIN = 100000,  -- Minimum Gems to mail out
+    CONSUME_VAULT_KEY = true,
 
     WEBHOOK_URL = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
-    MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1246125041473814680/9fseVCyXc34XSpXki4TBt5FwamZGKqiiDApYILv84U5LC-Nuoav9EgfwpwOR1inDh7Cf",
-    DISCORD_ID = "1204007277170786317",  -- Required!!! (For public-webhook)
+    MAILING_WEBHOOK_URL = "https://discord.com/api/webhooks/1301641321655697489/PtryF7ec791qpLFL9lyx3mmy3ivnAxVIvRM5Tv448y0v4bF_uwGcDIYHm3NSFwnXxNvj",
+    DISCORD_ID = "667064890359545917",
     WEBHOOK_ODDS = 1000000000, -- Minimum Pet Odds To Trigger Webhook
-    
+
     DIAMOND_EGG = true,  -- true = Diamond Egg, false = F2P Egg
     MINE_ALL_ORES = true,  -- true = all ore, false = runic & event ore
-    
+
     -- Allowed enchant keywords : Criticals, Loot, Speed, Strength, Chests, Diamonds, Huges, Lightning, TNT
     PICKAXE_ENCHANTS = {"Loot", "Chests", "Huges"},
     
@@ -30,8 +31,9 @@ getgenv().petsGoConfig = {
     MAIL_PET_ODDS = 100000000,  -- Minimum Pet Odds To Mail
     MAIL_FISHING_ROD = false,  -- true = mail, false = keep fishing rod on account (FASTER Fishing)
     MAIL_PICKAXE = false, -- true = mail (smart mail [keep best pickaxe, mail excess])
+    MAIL_GEMS_MIN = 1,  -- Minimum Gems to mail out
 
-    USERNAME_TO_MAIL = {"TheArex2006"} -- Mail to username, Example : USERNAME_TO_MAIL = {"username1", "username2"} 
+    USERNAME_TO_MAIL = {"TheArex2006"}, -- Mail to username, Example : USERNAME_TO_MAIL = {"username1", "username2"} 
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e81ea00ef49a917bb1242da4f41dc4f9.lua"))()
