3a4796be38e004c4a871a6b68977359448866e97
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-06-10 10:39:34 +0200

    Update pls.lua

--- pls.lua
+++ pls.lua
@@ -1,2 +1,2 @@
-script_key="gegVaftkIfAYYZbHgdeZhJPvqmuZtUVU";
+script_key="zpuBNPqhfvUIcpZLeAPnNafFqVdeZsPU";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ACjLrqZ32sB2eJJ/mAEqKnw5fv6hCiE/main/JmxaNHGqG4grTBE"))()
